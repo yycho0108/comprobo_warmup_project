d6b15449b0d410083f3e04053a4a2990deb88917
Jamie Cho <jchocholate@gmail.com>
2018-09-11 11:35:16 -0400

    theoretically functional square driver -- did not test

--- warmup_project/scripts/drive_square.py
+++ warmup_project/scripts/drive_square.py
@@ -3,30 +3,116 @@
 import rospy
 import tf
 from nav_msgs.msg import Odometry
+import numpy as np
+from tf_conversions import posemath as pm
+from geometry_msgs.msg import Pose, Pose2D, Twist
+
+def anorm(x):
+    return (x+np.pi) % (2*np.pi) - np.pi
+
+def adiff(a,b):
+    return anorm(a-b)
 
 class SquareDriver(object):
     def __init__(self):
         # receive ROS parameters
         self.rate_ = rospy.get_param('~rate', 50.0)
-        self.odom_from_tf = rospy.get_param('~odom_from_tf', False)
+        self.tf_mode_ = rospy.get_param('~tf_mode_', False)
+        self.gtol_ = rospy.get_param('~gtol', 5e-2) # goal tolerance
+        self.atol_ = rospy.get_param('~atol', np.deg2rad(10)) # waypoint angle tolerance
+
+        rospy.loginfo('Parameters : ')
+        rospy.loginfo('loop rate : {}'.format(self.rate_))
+        rospy.loginfo('goal tolerance : {}'.format(self.gtol_))
+        rospy.loginfo('control loop angular tolerance : {}'.format(self.atol_))
+        rospy.loginfo('tf mode : {}'.format(self.tf_mode_))
+
+        self._pose = None
 
         # create ROS handle
-        if self.odom_from_tf:
+        if self.tf_mode_:
             self.tfl_ = tf.TransformListener()
         else:
             self.odom_sub_ = rospy.Subscriber('odom', Odometry, self.odom_cb)
+        self.cmd_pub_ = rospy.Publisher('cmd_vel', Twist, queue_size=5)
+
+
+        self.n_phase_ = 4
+        self.phase_  = 0
+        self.phase_name_ = ['right', 'top', 'left', 'bottom']
+        self.target_ = [(1.0,-1.0),(1.0,1.0), (-1.0,1.0),(-1.0,-1.0)]
+        self.origin_ = None
+
+    @staticmethod
+    def to_pose2d(msg):
+        x, y  = [msg.position.x, msg.position.y]
+        rz = 2.0 * np.arctan2(msg.orientation.z, msg.orientation.w)
+        pose = Pose2D(x=x,y=y,theta=rz)
+        return pose
+
+    def get_odom(self):
+        try:
+            pose_tf = self.tfl_.lookupTransform('base_link', 'odom', rospy.Time(0))
+        except tf.Exception as e:
+            rospy.loginfo_throttle(1.0, 'Failed TF Transform : {}'.format(e) )
+            return None
+        msg = pm.toMsg(pm.fromTf(pose_tf))
+        pose = self.to_pose2d(msg)
+        return pose
     
     def odom_cb(self, msg):
-        pass
+        # nav_msgs/Odometry --> geometry_msgs/Pose2D
+        pose = self.to_pose2d(msg.pose.pose)
+        if self.origin_ is None:
+            self.origin_ = Pose2D(x=pose.x,y=pose.y,theta=pose.theta)
+        self.pose_ = Pose2D(x=pose.x,y=pose.y,theta=pose.theta)
 
     def run(self):
         rate = rospy.Rate(self.rate_)
+        cmd_vel = Twist()
         while not rospy.is_shutdown():
+            if (self.origin_ is None):
+                # need to initialize origin
+                if self.tf_mode_:
+                    pose = self.get_odom()
+                    if pose is not None:
+                        self.pose_   = Pose2D(x=pose.x,y=pose.y,theta=pose.theta)
+                        self.origin_ = Pose2D(x=pose.x,y=pose.y,theta=pose.theta)
+                else:
+                    # tf disabled, wait for odom callback
+                    pass
+            else:
+                # has origin
+                gx, gy = self.target_[self.phase_]
+                dx, dy = gx-self.pose_.x, gy-self.pose_.y
+                theta = np.arctan2(dy, dx)
+
+                if np.linalg.norm([dx,dy]) < self.gtol_:
+                    # reached goal!
+                    next_phase = (self.phase_ + 1) % self.n_phase_
+                    rospy.loginfo('Reached Goal! {} -> {}',
+                            self.phase_name_[self.phase_],
+                            self.phase_name_[next_phase])
+                    self.phase_ = next_phase
+                else:
+                    d_theta = adiff(theta, self.pose_.theta)
+                    if np.abs(d_theta) > self.atol_:
+                        # turn to align with goal first
+                        v = 0
+                        w = 0.2 * np.sign(d_theta) # TODO : pid or some loop closing action
+                    else:
+                        v = 0.2 # TODO : pid or some loop closing action
+                        w = 0.05 * d_theta # TODO : pid or some loop closing action
+                    cmd_vel.linear.x = v
+                    cmd_vel.angular.z = w
+                    self.cmd_pub_.publish(cmd_vel)
+
             rate.sleep()
 
 def main():
-    app = SquareDriver()
-    app.run()
+    rospy.init_node('comprobo_square_driver')
+    node = SquareDriver()
+    node.run()
 
 if __name__ == "__main__":
     main()
