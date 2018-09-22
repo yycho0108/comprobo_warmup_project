671e7b5d95339a26e0407a22055a7997a000963d
Jamie Cho <jchocholate@gmail.com>
2018-09-21 22:33:57 -0400

    hacking at obstacle avoidance, and I honestly think potential fields is not the right thing to do

--- warmup_project/scripts/obstacle_avoider.py
+++ warmup_project/scripts/obstacle_avoider.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python2
 
 import rospy
+import tf
 import numpy as np
+
+from tf_conversions import posemath as pm
 
 from warmup_project.pid import PID
 import warmup_project.utils as U
 
 from std_msgs.msg import ColorRGBA
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist, Point
+from geometry_msgs.msg import Twist, Point, Point32, Pose, PoseStamped, Polygon, PolygonStamped
 from visualization_msgs.msg import Marker
 
 
 class ObstacleAvoider(object):
     def __init__(self):
+        # hardcoded neato footprint
+        self.fpt_ = np.asarray([[ 0.25649121, -0.17213032],
+            [ 0.17213032, -0.17468672],
+            [ 0.0562406 , -0.17468672],
+            [-0.00596491, -0.14315788],
+            [-0.05027569, -0.09117794],
+            [-0.06987468, -0.00426065],
+            [-0.05027569,  0.07243107],
+            [ 0.00170426,  0.13804510],
+            [ 0.05624060,  0.16616541],
+            [ 0.16957393,  0.16957393],
+            [ 0.25478697,  0.16786967]], dtype=np.float32)
+
+        self.radius_ = rospy.get_param('~radius', 0.2) # todo : use "footprint" instead
+
+        # potential field params
+        self.a_ = rospy.get_param('~a', 0.5) # ??
+        self.b_ = rospy.get_param('~b', 0.01) # ??
+        self.r_ = rospy.get_param('~r', 0.25)
+        self.s_ = rospy.get_param('~s', 2.0)
 
         # initialize scan data fields
+        self.scan_fpt_ = None
         self.scan_ = None
         self.new_scan_ = False
         self.angle_ = None
@@ -24,23 +48,84 @@ class ObstacleAvoider(object):
         self.init_ = False
 
         self.viz_pub_ = rospy.Publisher('viz_pt', Marker, queue_size=2)
+        self.tfl_ = tf.TransformListener()
 
         self.pid_v_ = PID(kp=0.5, ki=0.03, max_u=0.5,max_i=0.25)
-        #self.k_v_ = 0.25 #??
-        self.pid_w_ = PID(kp=1.0, ki=0.08, max_u=1.0,max_i=0.5)
+        self.k_v_ = 0.2 #??
+        self.pid_w_ = PID(kp=1.0, ki=0.08, max_u=2.0,max_i=1.0)
         self.last_cmd_ = rospy.Time.now()
         self.cmd_pub_ = rospy.Publisher('cmd_vel', Twist, queue_size=5)
         self.cmd_vel_ = Twist()
-        self.scan_sub_ = rospy.Subscriber('scan', LaserScan, self.scan_cb)
+        self.scan_sub_ = rospy.Subscriber('stable_scan', LaserScan, self.scan_cb)
+
+        self.goal_ = None
+        self.goal_sub_ = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.goal_cb)
+        self.goal_viz_pub_ = rospy.Publisher('goal_area', PolygonStamped, queue_size=5)
+        self.goal_viz_msg_ = PolygonStamped()
+
+    def goal_cb(self, msg):
+        self.goal_ = msg.pose.position
+
+    def get_heading(self, source='odom', target='base_link'):
+        # pts = [N,2]
+        try:
+            #T_tf = self.tfl_.lookupTransform(target, source, rospy.Time(0))
+            T_tf = self.tfl_.lookupTransform(source, target, rospy.Time(0))
+        except tf.Exception as e:
+            rospy.logerr('Scan tf failed : {}'.format(e))
+            return None
+        T_msg = pm.toMsg(pm.fromTf(T_tf))
+        x,y = T_msg.position.x, T_msg.position.y
+        q   = 2.0 * np.arctan2(T_msg.orientation.z, T_msg.orientation.w)
+        #pts_odom = np.dot(pts, U.R(q).T) + np.reshape([x,y], [-1,2])
+        return q
 
     def scan_cb(self, msg):
         self.new_scan_ = True
         if not self.init_:
             self.angle_ = U.anorm(np.linspace(0,2*np.pi,len(msg.ranges),endpoint=True))
-            self.dmin_ = msg.range_min
+            #self.dmin_ = msg.range_min
+            self.dmin_ = max(msg.range_min, self.radius_) # -- radius
             self.dmax_ = msg.range_max
             self.init_ = True
+            self.scan_fpt_ = U.ScanFootprint(self.fpt_, self.angle_)
         self.scan_ = np.asarray(msg.ranges, dtype=np.float32)
+
+    def goal_viz(self):
+        if self.goal_ is None:
+            return
+
+        gx, gy = self.goal_.x, self.goal_.y
+        q = np.linspace(-np.pi, np.pi, num=10)
+        x = gx + self.r_ * np.cos(q)
+        y = gy + self.r_ * np.sin(q)
+        ar_r = np.stack([x,y], axis=-1)
+
+        x = gx + (self.r_+self.s_) * np.cos(q)
+        y = gy + (self.r_+self.s_) * np.sin(q)
+        ar_s = np.stack([x,y], axis=-1)
+
+        ar_g = np.concatenate([ar_r, ar_s], axis=0)
+
+        [cx,cy,_], _ = self.tfl_.lookupTransform('odom', 'base_link',  rospy.Time(0))
+        x = cx + self.r_ * np.cos(q)
+        y = cy + self.r_ * np.sin(q)
+        ar_r = np.stack([x,y], axis=-1)
+
+        x = cx + (self.r_+self.s_) * np.cos(q)
+        y = cy + (self.r_+self.s_) * np.sin(q)
+        ar_s = np.stack([x,y], axis=-1)
+
+        ar_b = np.concatenate([ar_r, ar_s], axis=0)
+
+        ar = np.concatenate([ar_g, ar_b], axis=0)
+
+
+
+        self.goal_viz_msg_.header.stamp = rospy.Time.now()
+        self.goal_viz_msg_.header.frame_id = 'odom'
+        self.goal_viz_msg_.polygon = Polygon([Point32(x,y,0) for (x,y) in ar])
+        self.goal_viz_pub_.publish(self.goal_viz_msg_)
 
     def cmd_viz(self, v):
         msg = Marker()
@@ -59,33 +144,110 @@ class ObstacleAvoider(object):
 
         self.viz_pub_.publish(msg)
 
-
     def step(self):
+        if self.goal_ is None:
+            return
+
+        self.goal_viz()
+
         if self.scan_ is not None:
+            scan_offset = self.scan_fpt_(self.scan_, scale=0.9)
             now = rospy.Time.now()
             dt = (now - self.last_cmd_).to_sec()
             rq = np.stack([self.scan_, self.angle_], axis=-1)
             mask = (self.dmin_ < self.scan_) & (self.scan_ < self.dmax_)
-            #mask &= (-np.pi/16 < self.angle_) & (self.angle_ < np.pi/16)
+
+            mask &= (scan_offset > 0) # internal-points rectification
+
             rq = rq[mask]
             if np.size(rq) <= 0:
                 return
 
+            h = self.get_heading()
+
+            # apply goal potential
+            [cx,cy,_], _ = self.tfl_.lookupTransform('odom', 'base_link',  rospy.Time(0))
+            gx, gy = self.goal_.x, self.goal_.y
+            d = np.linalg.norm([gx-cx,gy-cy])
+            q = np.arctan2(gy-cy,gx-cx)
+
+            dx,dy = 0,0
+            if d < self.r_:
+                dx=0
+                dy=0
+                rospy.loginfo("Reached Goal!")
+                self.goal_ = None
+                self.cmd_pub_.publish(Twist())
+                return
+            elif d <= self.r_ + self.s_:
+                dx = self.a_ * (d - self.r_) * np.cos(q)
+                dy = self.a_ * (d - self.r_) * np.sin(q)
+            else:
+                dx = self.a_ * self.s_ * np.cos(q)
+                dy = self.a_ * self.a_ * np.sin(q)
+
+            # save results
+            gdx = dx # dx,dy are in odom coordinates !
+            gdy = dy
+
             r, q = rq.T
+            q = U.anorm(q+h) # rotate to align obstacle points with odom coordinates
 
-            weight  = (1.0 / np.abs(r))
-            dx = np.cos(q)
-            dy = np.sin(q)
+            obs = scan_offset[mask] < 0.05
+            if np.any(obs):
+                rospy.loginfo("obs!")
+            n_obs = np.logical_not(obs)
+            out = (r > (self.s_ + self.r_))
 
-            uv = np.reshape(weight, [-1,1]) * np.stack([-dx,-dy], axis=-1) #N,2
-            uv = np.sum(uv, axis=0, keepdims=True)
-            uv /= np.linalg.norm(uv, axis=-1)
+            dx = np.zeros_like(r)
+            dy = np.zeros_like(r)
 
+            dx[obs] = - 99999. * np.sign(np.cos(q[obs]))
+            dy[obs] = - 99999. * np.sign(np.sin(q[obs]))
+
+            dx[n_obs] = - self.b_ * (self.s_ + self.r_ - r[n_obs]) * np.cos(q[n_obs])
+            dy[n_obs] = - self.b_ * (self.s_ + self.r_ - r[n_obs]) * np.sin(q[n_obs])
+
+            dx[out] *= 0
+            dy[out] *= 0
+
+            uv = np.sum(np.stack([dx,dy], axis=-1), axis=0, keepdims=True)
+            uv += [[gdx,gdy]]
+            #uv /= np.linalg.norm(uv, axis=-1)
+
+            #uv = np.reshape(weight, [-1,1]) * np.stack([dx,dy], axis=-1) #N,2
+            #uv = np.sum(uv, axis=0, keepdims=True)
+            #uv += [[gdx,gdy]] # (1,2)
+            #uv /= np.linalg.norm(uv, axis=-1)
+
+            #weight[obs] = np.sign(np.cos(q))
+
+            #if np.any(obs):
+            #    rospy.loginfo("Obstacle!")
+            #    uq = np.mean( U.anorm(q[obs]) )
+            #    uv = np.reshape([-np.cos(uq), -np.sin(uq)], [1,2])
+            #else:
+            #    weight = self.b_ * (self.s_ + self.r_ - r)
+            #    weight *= (r < (self.s_ + self.r_) ) # out of influence
+            #    dx = np.cos(q)
+            #    dy = np.sin(q)
+
+            #    uv = np.reshape(weight, [-1,1]) * np.stack([dx,dy], axis=-1) #N,2
+            #    uv = np.sum(uv, axis=0, keepdims=True)
+            #    uv += [[gdx,gdy]] # (1,2)
+            #    uv /= np.linalg.norm(uv, axis=-1)
+
+            uv = uv.dot(U.R(h)) # back to base_link - note R(h) transposed twice
             self.cmd_viz(uv[0])
 
-            _, v_q = U.xy2rq(uv)[0]
+            v_v, v_q = U.xy2rq(uv)[0]
 
-            v = self.pid_v_(0.4 / np.mean(r) ,dt) #self.k_v_ * (np.min(r))#, dt)
+            #v = self.pid_v_(0.4 / np.mean(r) ,dt) #self.k_v_ * (np.min(r))#, dt)
+            #if uv[0,0] < 0.0: # negative
+            #    v = 0.0
+            #else:
+            #    v = self.k_v_# * np.min(r)
+            v = self.pid_v_(v_v, dt)
             w = self.pid_w_(v_q, dt)
 
             self.cmd_vel_.linear.x = v
--- warmup_project/src/warmup_project/utils.py
+++ warmup_project/src/warmup_project/utils.py
@@ -19,9 +19,108 @@ def xy2rq(xy):
     q = np.arctan2(y,x)
     return np.stack([r,q], axis=-1)
 
+def inv_22(A):
+    a = A[0,0]
+    b = A[0,0]
+    c = A[0,0]
+    d = A[0,0]
+    D = np.linalg.det(A)
+
+def uvec(x, axis=-1):
+    return x / np.linalg.norm(x, axis=axis)
+
 def R(x):
     c = np.cos(x)
     s = np.sin(x)
     r = [[c,-s],[s,c]]
     return np.asarray(r, dtype=np.float32)
+
+#def seg_ix(seg0, seg1):
+#    (x1,y1), (x2,y2) = seg0
+#    (x3,y3), (x4,y4) = seg1
+#
+#    ta_n = (y3-y4)*(x1-x3)+(x4-x3)*(y1-y3)
+#    ta_d = (x4-x3)*(y1-y2)-(x1-x2)*(y4-y3)
+#    ta = (ta_n / ta_d)
+#
+#    tb_n=(y1-y2)*(x1-x3)+(x2-x1)*(y1-y3)
+#    tb_d=(x4-x3)*(y1-y2)-(x1-x2)*(y4-y3)
+#    tb = (tb_n / tb_d)
+#
+#    return ta, tb
+
+def lineRayIntersectionPoint(rayOrigin, rayDirection, point1, point2):
+    """
+    from https://stackoverflow.com/a/29020182
+    """
+
+    # Convert to numpy arrays
+    rayOrigin = np.array(rayOrigin, dtype=np.float)
+    rayDirection = np.array(uvec(rayDirection), dtype=np.float32)
+    point1 = np.array(point1, dtype=np.float)
+    point2 = np.array(point2, dtype=np.float)
+
+    # Ray-Line Segment Intersection Test in 2D
+    # http://bit.ly/1CoxdrG
+    v1 = rayOrigin - point1
+    v2 = point2 - point1
+    v3 = np.array([-rayDirection[1], rayDirection[0]])
+    if np.dot(v2,v3) == 0:
+        return None
+    t1 = np.cross(v2, v1) / np.dot(v2, v3)
+    t2 = np.dot(v1, v3) / np.dot(v2, v3)
+    if t1 >= 0.0 and t2 >= 0.0 and t2 <= 1.0:
+        return rayOrigin + t1 * rayDirection
+    return None
+
+class ScanFootprint(object):
+    """
+    Transform scan to represent offset from robot border.
+    """
+    def __init__(self, fpt, ang):
+        self.fpt_ = fpt
+        self.ang_ = ang
+        self.dr_ = np.zeros_like(self.ang_)
+
+        n = len(fpt)
+        for i in range(-1,n-1):
+            pa, pb = self.fpt_[i], self.fpt_[i+1]
+            for ai, h in enumerate(ang):
+                c,s = np.cos(h), np.sin(h)
+                ix = lineRayIntersectionPoint([0,0], [c,s], pa, pb)
+                if ix is not None:
+                    self.dr_[ai] = np.linalg.norm(ix)
+
+    def show(self):
+        from matplotlib import pyplot as plt
+        c = np.cos(self.ang_)
+        s = np.sin(self.ang_)
+        x = self.dr_ * c
+        y = self.dr_ * s
+        plt.scatter(x,y)
+        plt.show()
+
+    def __call__(self, scan, scale=1.0):
+        return scan - (self.dr_ * scale)
+
+def main():
+    fpt = np.asarray([[ 0.25649121, -0.17213032],
+        [ 0.17213032, -0.17468672],
+        [ 0.0562406 , -0.17468672],
+        [-0.00596491, -0.14315788],
+        [-0.05027569, -0.09117794],
+        [-0.06987468, -0.00426065],
+        [-0.05027569,  0.07243107],
+        [ 0.00170426,  0.13804510],
+        [ 0.05624060,  0.16616541],
+        [ 0.16957393,  0.16957393],
+        [ 0.25478697,  0.16786967]], dtype=np.float32)
+    ang = anorm(np.linspace(0,2*np.pi,360,endpoint=True))
+    sfp = ScanFootprint(fpt,ang)
+    sfp.show()
+
+if __name__ == "__main__":
+    main()
+
+
 
